7d6b8f03d48de9a9c32907152e1b98c9e522d0a4
Yash kumar <yashkumar@Yashs-MacBook-Air.local>
2024-11-21 14:30:31 +0530

    Switched from bcrypt to bcryptjs for compatibility

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -2,7 +2,7 @@
 const User = require("../models/User");
 const mailSender = require("../utilis/mailSender");
 const resetPasswordTemplate = require("../mail/templates/resetPassword")
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
 //  reset password token  
